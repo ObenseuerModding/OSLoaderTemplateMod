b3998a312ab932c47ba8f192eb7b5f2973d2134c
Robyn <rnixon98@gmail.com>
2024-12-04 11:52:13 -0600

    initial commit

---
+++ src/OSLoaderModTemplate/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using OSLoader;
+using UnityEngine;
+using OSLoaderModTemplate.Settings;
+
+namespace OSLoaderModTemplate;
+
+public class TestMod : Mod
+{
+    public static TestMod instance;
+
+    public override void InitializeMod()
+    {
+        instance = this;
+        settings = new CustomSettingsExample();
+    }
+
+    public override void OnModInitialized()
+    {
+        Debug.Log("This amazing mod was loaded!");
+    }
+
+    public override bool UnloadMod()
+    {
+        return false;
+    }
+}
